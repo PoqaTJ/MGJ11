6e6c83eda393fc4e2c83bc930402fb7744e14a1d
PoqaTJ <Tbjohnson42@gmail.com>
2025-02-09 10:35:55 -0800

    Add localization system. Set up title screen.

---
+++ Assets/Scripts/Menus/Localization/LocalizationSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+namespace Menus.Localization
+{
+    public class LocalizationSelector : MonoBehaviour
+    {
+        public void ChangeLocale(int localeID)
+        {
+            StartCoroutine(SetLocale(localeID));
+        }
+        
+        IEnumerator SetLocale(int localeId)
+        {
+            yield return LocalizationSettings.InitializationOperation;
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeId];
+        }
+    }
+}
