fe7889e27f45c35cd96e26a99972028cc5d0bb25
PoqaTJ <Tbjohnson42@gmail.com>
2025-02-09 18:03:17 -0800

    Add gameplay scene

---
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using Services;
+using Game;
+using UnityEngine;
+
+namespace Menus
+{
+    public class MainMenuController: MonoBehaviour
+    {
+        public void StartPressed()
+        {
+            ServiceLocator.Instance.GameManager.SetState(State.Gameplay);
+        }
+    }
+}
