87f352eb6ce0641dd8f960145e79e0be437da667
PoqaTJ <Tbjohnson42@gmail.com>
2025-02-12 20:19:06 -0800

    Fix issue where z's get flipped when turning around, ruining ordering.

---
+++ Assets/Scripts/Animation/FixDepthAfterRotation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animation
+{
+    public class FixDepthAfterRotation: MonoBehaviour
+    {
+        private float posZ;
+
+        void Start()
+        {
+            posZ = transform.position.z;
+        }
+
+        private void LateUpdate()
+        {
+            if (transform.position.z != posZ)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
+            }
+        }
+    }
+}
