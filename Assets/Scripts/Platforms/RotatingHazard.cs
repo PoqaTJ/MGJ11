b71b3725c9e804b9a55f7940968e7439f53e3a6e
PoqaTJ <Tbjohnson42@gmail.com>
2025-02-24 20:39:24 -0800

    Update title screen art

--- Assets/Scripts/Platforms/RotatingHazard.cs
+++ Assets/Scripts/Platforms/RotatingHazard.cs
@@ -5,11 +5,11 @@ namespace Platforms
 {
     public class RotatingHazard: MonoBehaviour
     {
-        [SerializeField] private float _rotationSpeed = 1f;
+        [SerializeField] private float _speed1 = 10f;
 
         private void Update()
         {
-            transform.Rotate(new Vector3(0, 0, _rotationSpeed));
+            transform.Rotate(new Vector3(0, 0, _speed1 * Time.deltaTime));
         }
     }
 }
